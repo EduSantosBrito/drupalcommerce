35a3a8cc27ab7b0a358d1d48648ea0d405f50bbb
Alessandro <alessandromacanha@gmail.com>
2017-09-27 15:00:11 -0300

    Melhorada area admin

--- build/web/Shared/js/index.js
+++ build/web/Shared/js/index.js
@@ -1,22 +1,18 @@
 $( document ).ready(function() {
-        $('.carousel').carousel({
-                dist: 0,
-                padding: 20,
-        });
         $("#large-navbar").hide();
         $(".button-collapse").sideNav();
-        $('.collapsible').collapsible();
-        $('.dropdown-button').dropdown({
+        $(".collapsible").collapsible();
+        $(".dropdown-button").dropdown({
         constrainWidth: false, // Does not change width of dropdown to that of the activator
         gutter: 42, // Spacing from edge
         belowOrigin: false, // Displays dropdown below the button
         alignment: 'left', // Displays dropdown with edge aligned to the left of button
         stopPropagation: true // Stops event propagation
     });
-        $('.dropdown-button + .dropdown-content').on('click', function(event) {
+        $(".dropdown-button + .dropdown-content").on("click", function(event) {
                 event.stopPropagation();
         });
-        $('.dropdown-button-login').dropdown({
+        $(".dropdown-button-login").dropdown({
         constrainWidth: false, // Does not change width of dropdown to that of the activator
         gutter: 0, // Spacing from edge
         belowOrigin: true, // Displays dropdown below the button
@@ -25,20 +21,10 @@ $( document ).ready(function() {
     });	
 });
 $("#small-bar").click(function() {
-        $("#small-navbar").hide();
-        $("#large-navbar").fadeIn("slow");
+    $("#small-navbar").hide();
+    $("#large-navbar").fadeIn("slow");
 });
 $("#large-bar").click(function() {
-        $("#large-navbar").hide();
-        $("#small-navbar").fadeIn("slow");
-});
-$('.moveNextCarousel').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.carousel').carousel('next');
-});
-$('.movePrevCarousel').click(function(e){
-e.preventDefault();
-e.stopPropagation();
-$('.carousel').carousel('prev');
+    $("#large-navbar").hide();
+    $("#small-navbar").fadeIn("slow");
 });
